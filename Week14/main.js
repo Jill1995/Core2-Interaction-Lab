aa84c8c75e13ead40dc4c94b1848595f86fa8436
Jill1995 <53446341+Jill1995@users.noreply.github.com>
2022-05-09 09:05:38 -0400

    class code updated

--- Week14/main.js
+++ Week14/main.js
@@ -1,39 +1,87 @@
-const h2 = document.getElementById("changing-text");
-const img = document.getElementById("changing-img");
+let title = document.getElementById("changing-text");
+let image = document.getElementById("changing-img");
+// const btn = document.getElementById("addimage");
 
-document.addEventListener('keydown', (ev)=>{
-    h2.innerHTML +=  ev.key;
-    setImage(h2.innerHTML);
-    // if(h2.innerHTML == "earth"){
-    //     img.src = "earth.png";
-    // }
-})
+let keyspressed = []
 
-document.addEventListener('keyup', (ev)=>{
-    if(ev.key === "Backspace"){
-        console.log(ev.code);
-        h2.innerHTML = ""
+// let codeArray = ["e","g","y","u","h","j"];
+let interval = setInterval(changeBackground, 1000);
+
+// btn.addEventListener('click', function(){
+//     let img2 = document.createElement("img");
+//     img2.className = "imagesadded";
+//     img2.src = "earth.png";
+//     document.body.appendChild(img2);
+// })
+
+document.addEventListener('keydown', function(evt){
+    // console.log(evt.code, evt.key);
+    if(evt.metaKey == false){
+        //clearInterval(interval);
+        title.innerHTML += evt.key;
+        setImage(title.innerHTML);
+        keyspressed.push(evt.key);
+
+        if(evt.key == 'p'){
+            clearInterval(interval);
+        }
     }
 
-    // if(ev.code === "KeyE"){
-    //     img.src = "earth.png";
-    // }else if(ev.code === "KeyM"){
-    //     img.src = "moon.png";
-    // }else if(ev.code === "KeyS"){
-    //     img.src = "sun.png";
-    // }
-})
+});
 
 function setImage(word){
-    if(word == "earth"){
-        img.src = "earth.png";
-    }
-    else if(word == "moon"){
-        img.src = "moon.png";
-    }
-    else if(word == "sun"){
-        img.src = "sun.png";
-    }else{
-        img.src ="";
+    switch(word){
+        case 'earth':
+            image.src = "earth.png";
+            break;
+        case 'moon':
+            image.src = "moon.png";
+            break;
+        case 'sun':
+            image.src = "sun.png";
+            break;
+        default:
+            image.src = "";
     }
 }
+
+document.addEventListener('keyup', function(evt){
+
+    if(evt.key === "Backspace"){
+        console.log(evt.code);
+        title.innerHTML = ""
+    }
+
+    if(evt.key === "Alt"){
+        title.classList.toggle("change");
+    }
+
+    if(evt.key === "Control"){
+        title.classList.toggle("before-change");
+    }
+
+    // switch(evt.key){
+    //     case 'e':
+    //         //title.innerHTML = codeArray[5];
+    //         image.src = "earth.png";
+    //         break;
+    //     case 'm':
+    //         //title.innerHTML = codeArray[0];
+    //         image.src = "moon.png";
+    //         break;
+    //     case 's':
+    //         //title.innerHTML = codeArray[1];
+    //         image.src = "sun.png";
+    //         break;
+    //     default:
+    //         image.src = "";
+    // }
+})
+
+
+function changeBackground(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
+    document.body.style.background ="#" + randomColor;
+}
+
